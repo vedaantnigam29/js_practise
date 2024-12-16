24a3bfc9de01320f81ad95632e9c27226d3ca04c
vedaantnigam29 <vedaantnigamiitian@gmail.com>
2024-12-16 20:58:04 +0530

    strings

--- 001_basics/string.js
+++ 001_basics/string.js
@@ -10,7 +10,15 @@ console.log(gameName.length);
 console.log(gameName.toUpperCase());
 const newString = gameName.substring(0,4);
 console.log(newString);
+const anotherstring = gameName.slice(-6,4)
 
+const newStringOne = "   vedaant   "
+console.log(newStringOne.trim());
+const url = "https://ved.com/ved%20nig"
+console.log(url.replace('%20','-'))
+console.log(url.includes('ved'));
+console.log(gameName.split('-'));
+console.log(url.concat(gameName,url));
 
 
 

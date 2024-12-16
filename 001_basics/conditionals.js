c04cdfb3d44849514a3d8bc99149da01dd49c62c
vedaantnigam29 <vedaantnigamiitian@gmail.com>
2024-12-16 19:06:02 +0530

    conditionals

---
+++ 001_basics/conditionals.js
@@ -0,0 +1,19 @@
+console.log(2>1);
+console.log(null>0);
+console.log(null>=0)
+
+// strict check: ===
+console.log("2"===2); //false is the output
+// primitive(call by value)
+
+// 7 types :string,number,boolean,null,undefined,symbol,BigInt
+
+// Reference type(call by ref)
+
+// arrays,objects,functions
+const score=100
+const scoreval = 100.3
+const isLoggedIn = false
+const bignumber = 3456782345673456784567n
+  
+

adb5ac581430627a457f43943d130df655e858ec
vedaantnigam29 <vedaantnigamiitian@gmail.com>
2024-12-18 12:55:31 +0530

    objects

--- 001_basics/objects.js
+++ 001_basics/objects.js
@@ -18,11 +18,47 @@ jsuser.greeting  = function(){
     console.log("hello js user");
     
 }
-console.log(jsuser.greeting(  ));
+console.log(jsuser.greeting());
 jsuser.greetingtwo = function() {
-    console.log('hello ${}');
+    console.log('hello ${this.name}');
     
 }
+console.log(jsuser.greetingtwo());
+/**********part 2***********/
+const tinderuser1 = new Object()//singleton
+const tinderuser = {}//non singleton
+
+tinderuser.id = "123abc"
+tinderuser.name = "sam"
+tinderuser.isLoggedIn = false
+console.log(tinderuser);
+
+const regularuser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname:{
+            firstname: "vedaant",
+            lastname:"nigam"
+        }
+
+    }
+}
+ console.log(regularuser.fullname.userfullname);
+ 
+ const obj1 = {1: "a", 2:"b"}
+ const obj2 = {3:"c", 4:"d"}
+ //const obj3 = Object.assign({},obj1,obj2)//empty parenthesis is formal apporach.it is an empty object where all the soruce is stored
+ const obj3 = {...obj1,obj2}
+ console.log(obj3);
+ console.log(tinderuser);
+
+ console.log(Object.keys(tinderuser));
+ console.log(Object.values(tinderuser));
+ console.log(tinderuser.hasOwnProperty('isLoggedIn'));
+ 
+ 
+ 
+ 
 
 
 

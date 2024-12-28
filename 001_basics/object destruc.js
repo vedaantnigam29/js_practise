4528201e66327f22cc3333cd94c568ab069ee569
vedaantnigam29 <vedaantnigamiitian@gmail.com>
2024-12-28 12:52:28 +0530

    obj destruc

---
+++ 001_basics/object destruc.js
@@ -0,0 +1,14 @@
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+const {courseInstructor: instructor} = course
+console.log(instructor);
+
+//{
+ //   "name": "hitesh",
+   // "coursename": "js in hindi",
+    //"price": "free"
+//} //this is jason
+

dbe82192d98b6d047fff1fb39a63a656dcba6c77
vedaantnigam29 <vedaantnigamiitian@gmail.com>
2024-12-29 14:03:20 +0530

    iife

---
+++ 001_basics/imm invoked func.js
@@ -0,0 +1,11 @@
+(function chai(){
+    console.log(`db connected`);
+    
+})(); // semi colon neccessary
+// (this perenthesis for func describtion)(this one is for execution)
+(
+    (name)=>{
+        console.log(`db connected two ${name}`);
+        
+    }
+)("vedaant");
